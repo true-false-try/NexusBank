ac21dd1ab5234e98aaca45974c7e594563aa3f0a
true-false-try <6mateo6ch@gmail.com>
2026-07-21 16:24:58 +0300

    first commit

---
+++ src/main/java/com/bank/nexus/NexusBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexusBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bank/nexus/NexusBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
